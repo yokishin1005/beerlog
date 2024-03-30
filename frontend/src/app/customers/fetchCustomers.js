da0a819bb2e067c16ed554e54f24c8276127a3ab
RockyIwata <149314865+RockyIwata@users.noreply.github.com>
2024-03-30 14:20:51 +0900

    Add files via upload

---
+++ frontend/src/app/customers/fetchCustomers.js
@@ -0,0 +1,8 @@
+export default async function fetchCustomers() {
+    const res = await fetch(process.env.API_ENDPOINT+'/allcustomers', { cache: "no-cache" });
+    if (!res.ok) {
+      throw new Error('Failed to fetch customers');
+    }
+    return res.json();
+  }
+  
