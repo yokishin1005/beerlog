50c344514d0d0cc632b89c45896f0f1466e345cb
yokishin1005 <yo.kishimoto9@gmail.com>
2024-04-23 23:17:26 +0900

    modified:   frontend/src/app/login/page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch(`${process.env.API_ENDPOINT}/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
